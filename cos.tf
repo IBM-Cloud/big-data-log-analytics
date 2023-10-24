4e59d7a9ca39273b0f4270eb75daa26be8b648fd
Frederic Lavigne <frederic.lavigne@ibm.com>
2023-10-24 17:26:14 -0500

    add solution.py

--- cos.tf
+++ cos.tf
@@ -33,7 +33,6 @@ resource "ibm_cos_bucket" "bucket" {
   kms_key_crn          = ibm_kms_key.root.crn
 }
 
-
 resource "ibm_cos_bucket_object" "hello" {
   bucket_crn = ibm_cos_bucket.bucket.crn
   bucket_location = ibm_cos_bucket.bucket.region_location
@@ -44,5 +43,40 @@ def main():
 
 if __name__ == '__main__':
   main()
+EOT
+}
+
+resource "ibm_cos_bucket_object" "solution" {
+  bucket_crn = ibm_cos_bucket.bucket.crn
+  bucket_location = ibm_cos_bucket.bucket.region_location
+  key = "solution.py"
+  content = <<EOT
+from pyspark.sql import SparkSession
+from pyspark.sql import SQLContext
+import os
+import sys
+
+def main():
+  # COS_PARQUET in format: cos://<bucket>.<service>/landing_folder/topic=<topic>/jobid=<jobid>/
+  # like cos://ABC-log-analysis.solution/logs-stream-landing/topic=webserver/jobid=48914a16-1d33-4d3e-93e3-7efb855b662e/
+  print("solution v1.0")
+  if "COS_PARQUET" not in os.environ:
+    print("COS_PARQUET must be in environment")
+    return 1
+  cos_parquet = os.environ["COS_PARQUET"]
+  print(f"cos_parquet: {cos_parquet}")
+
+  spark = SparkSession.builder.appName("solution").getOrCreate()
+  sc = spark.sparkContext
+  sqlContext = SQLContext(sc)
+
+  df = spark.read.parquet(cos_parquet)
+  sqlContext.registerDataFrameAsTable(df, "Table")
+  df_query = sqlContext.sql("SELECT * FROM Table LIMIT 10")
+  df_query.show(10)
+  return 0
+
+if __name__ == '__main__':
+  sys.exit(main())
 EOT
 }

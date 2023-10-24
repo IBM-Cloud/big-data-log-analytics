44688556be7e1704fafbc7150f221f9c3dfed80c
Frederic Lavigne <frederic.lavigne@ibm.com>
2023-10-24 15:48:04 -0500

    use curl until schematics issue is fixed

--- event-streams.tf
+++ event-streams.tf
@@ -7,18 +7,35 @@ resource "ibm_resource_instance" "es" {
   tags              = var.tags
 }
 
-resource "ibm_event_streams_topic" "webserver" {
-  name                 = "webserver"
-  partitions           = 1
-  resource_instance_id = ibm_resource_instance.es.crn
-  config = {
-    "retention.ms" = "86400000"
-  }
-}
-
 resource "ibm_resource_key" "es-for-log-analysis" {
   name                 = "es-for-log-analysis"
   role                 = "Writer"
   resource_instance_id = ibm_resource_instance.es.crn
   tags                 = var.tags
+}
+
+# until https://github.com/IBM-Cloud/terraform-provider-ibm/issues/4878 is solved
+# resource "ibm_event_streams_topic" "webserver" {
+#   name                 = "webserver"
+#   partitions           = 1
+#   resource_instance_id = ibm_resource_instance.es.crn
+#   config = {
+#     "retention.ms" = "86400000"
+#   }
+# }
+
+# workaround for https://github.com/IBM-Cloud/terraform-provider-ibm/issues/4878
+# https://cloud.ibm.com/apidocs/event-streams/adminrest#createtopic
+resource "null_resource" "event_streams_webserver_topic" {
+  provisioner "local-exec" {
+    command = join(" ", [
+      "curl -i -X POST",
+      "-H 'Accept: application/json'",
+      "-H 'Content-Type: application/json'",
+      "-H 'Authorization: ${data.ibm_iam_auth_token.tokendata.iam_access_token}'",
+      "--data",
+      "'{ \"name\": \"webserver\", \"partitions\": 1}'",
+      "${ibm_resource_key.es-for-log-analysis.credentials.kafka_admin_url}/admin/topics"
+    ])
+  }
 }
